d64701a7bc823dbc95b3a6faccb3f0bc3c9e2c2c
System Administrator <root@Sachins-MacBook-Pro.local>
2020-09-12 03:30:37 -0700

    update git socket

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+
+  	//request is received
+  	//processing
+
+  	msg.join = "approved"
+
     io.emit('chat message', msg);
+    
+    //
+    console.log(msg.joinvideo);
+    console.log(msg.userid);
+
+
   });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
